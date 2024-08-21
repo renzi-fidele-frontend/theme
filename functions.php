c52883072a583d9799b0ff8408d0b5058cd23c4d
Renzi Fidele <renzifidele2001@gmail.com>
2024-08-21 15:06:37 +0200

    First commit

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+function enqueue_swiperjs() {
+    // Enqueue SwiperJS CSS
+    wp_enqueue_style( 'swiperjs-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', array(), '11.0.0' );
+
+    // Enqueue SwiperJS JavaScript
+    wp_enqueue_script( 'swiperjs-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), '11.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_swiperjs' );
+
+function my_child_theme_enqueue_styles() {
+    // Enqueue parent theme's stylesheet
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    
+    // Enqueue child theme's stylesheet
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('parent-style') );
+}
+add_action( 'wp_enqueue_scripts', 'my_child_theme_enqueue_styles' );
