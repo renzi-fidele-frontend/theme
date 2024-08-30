ba9b11ff31b1bd22e2fbba2ab0ff63f951215ddd
Renzi Fidele <renzifidele2001@gmail.com>
2024-08-30 23:21:04 +0200

    Corrigindo o responsivo de ambos carouseis

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
    const swiper = new Swiper("#SwiperParceiro", {
       slidesPerView: "auto",
-      breakpoints: { 500: { slidesPerView: 2 }, 1200: { slidesPerView: 5 } },
+      breakpoints: { 1200: { slidesPerView: 5 } },
       slideToClickedSlide: true,
       rewind: true,
       centeredSlides: true,
@@ -14,9 +14,9 @@ document.addEventListener("DOMContentLoaded", function () {
    const swipertest = new Swiper("#SwiperTestemunhos", {
       slidesPerView: "auto",
       spaceBetween: 40,
-      breakpoints: { 700: { slidesPerView: 2 }, 1200: { slidesPerView: "auto" } },
+      breakpoints: { 1200: { slidesPerView: "auto" } },
       rewind: true,
-      slidesCentered: true,
+      centeredSlides: true,
       slideToClickedSlide: true,
       navigation: {
          nextEl: ".swiper-button-next",
